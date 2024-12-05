e610fdbd7acb9f0c835d5ca948cd35b74412174e
yunsangee <michael0622@naver.com>
2024-12-05 11:54:35 +0900

    12.05 first commit

---
+++ src/test/java/site/opgg/OpggApplicationTests.java
@@ -0,0 +1,13 @@
+package site.opgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
